25c6af6ee99ee3b6c1a070e92bf7689df4682c0d
Marshall Ovierdo <shrallvierdo@gmail.com>
2022-08-27 10:27:06 +0700

    feat: mobile view

--- src/App.vue
+++ src/App.vue
@@ -2,21 +2,23 @@
   <div class="bg-light w-screen min-h-screen">
     <div
       id="navbar"
-      class="w-full flex items-center gap-4 font-nunito sticky px-36"
+      class="w-full flex items-center gap-4 font-nunito sticky px-4 xl:px-36 py-2 xl:py-0"
     >
-      <img src="./assets/logo.svg" class="w-24 h-24" />
-      <div class="text-3xl font-bold">Diary Karakter</div>
+      <img src="./assets/logo.svg" class="xl:w-24 xl:h-24 w-12 h-12" />
+      <div class="text-md xl:text-3xl font-bold">Diary Karakter</div>
       <a
         href="#"
         target="_blank"
         class="
-          text-kasih-350 text-3xl
+          text-kasih-350 text-md xl:text-3xl
           border-2
           ml-auto
           border-kasih-350
           rounded-xl
-          px-4
-          py-2
+          px-2
+          py-1
+          xl:px-4
+          xl:py-2
           hover:bg-kasih-350
           cursor-pointer
           hover:text-light
@@ -24,9 +26,9 @@
         >Download Sekarang</a
       >
     </div>
-    <div class="grid grid-cols-12 items-center my-24 px-36">
-      <div class="col-span-3 flex flex-col gap-4">
-        <div class="text-3xl font-bold">
+    <div class="grid grid-cols-12 items-center my-24 px-4 xl:px-36">
+      <div class="col-span-12 xl:col-span-3 flex flex-col items-center xl:items-start gap-4">
+        <div class="text-md xl:text-3xl font-bold">
           Yuk isi Diary harianmu dengan Diary Karakter
         </div>
         <div class="text-xl">
@@ -43,28 +45,28 @@
             px-6
             py-3
             w-fit
-            text-3xl
+            text-md xl:text-3xl
             cursor-pointer
             hover:opacity-80
           "
           >Coba Sekarang</a
         >
       </div>
-      <div class="col-span-9">
+      <div class="col-span-12 xl:col-span-9 mt-4 xl:mt-0">
         <img src="./assets/landing-1.svg" alt="" srcset="" />
       </div>
     </div>
     <div
-      class="grid grid-cols-12 items-center my-24 bg-kasih-350 text-light px-36"
+      class="grid grid-cols-12 items-center my-24 bg-kasih-350 text-light px-4 xl:px-36 py-12 xl:py-0"
     >
-      <div class="col-span-9 absolute">
+      <div class="col-span-12 xl:col-span-9 xl:absolute">
         <img src="./assets/landing-2.svg" alt="" srcset="" />
       </div>
-      <div class="col-span-8 invisible h-148">
+      <div class="col-span-8 invisible hidden xl:block h-148">
         <img src="./assets/landing-2.svg" />
       </div>
-      <div class="col-span-4 flex flex-col gap-4">
-        <div class="text-3xl font-bold">
+      <div class="col-span-12 xl:col-span-4 flex flex-col items-center xl:items-start gap-4 pt-4 xl:pt-0">
+        <div class="text-md xl:text-3xl font-bold">
           Berbagai pilihan modul dalam memahami karakter
         </div>
         <div class="text-xl">
@@ -81,7 +83,7 @@
             px-6
             py-3
             w-fit
-            text-3xl
+            text-md xl:text-3xl
             cursor-pointer
             hover:opacity-80
           "
@@ -89,12 +91,12 @@
         >
       </div>
     </div>
-    <div class="grid grid-cols-12 items-center my-24 pl-36 pr-12 pb-12">
-      <div class="col-span-5">
+    <div class="grid grid-cols-12 items-center my-24 pl-12 xl:pl-36 xl:pr-12 pb-12">
+      <div class="col-span-12 xl:col-span-5">
         <div class="flex flex-col gap-4 pr-12">
           <div class="bg-white rounded-xl p-6 flex flex-col gap-4">
             <img src="./assets/bxs-widget.svg.svg" class="w-20 h-20" />
-            <div class="text-3xl font-bold">
+            <div class="text-md xl:text-3xl font-bold">
               Pilih Modul yang Akan Dikerjakan
             </div>
             <div class="text-xl">
@@ -104,7 +106,7 @@
           </div>
           <div class="bg-white rounded-xl p-6 flex flex-col gap-4">
             <img src="./assets/bxs-book-heart.svg.svg" class="w-20 h-20" />
-            <div class="text-3xl font-bold">Pilih Diary Harian</div>
+            <div class="text-md xl:text-3xl font-bold">Pilih Diary Harian</div>
             <div class="text-xl">
               Dalam setiap modul terdapat berbagai macam karakter yang dalam
               setiap karakternya memiliki diary harian yang perlu diselesiaikan
@@ -112,14 +114,14 @@
           </div>
           <div class="bg-white rounded-xl p-6 flex flex-col gap-4">
             <img src="./assets/bxs-comment-edit.svg.svg" class="w-20 h-20" />
-            <div class="text-3xl font-bold">Menyelesaikan Diary Harian</div>
+            <div class="text-md xl:text-3xl font-bold">Menyelesaikan Diary Harian</div>
             <div class="text-xl">
               Terdapat berbagai soal yang akan ditanyakan pada diary harian
             </div>
           </div>
         </div>
       </div>
-      <div class="col-span-7">
+      <div class="col-span-12 xl:col-span-7 pt-4 xl:pt-0">
         <img src="./assets/landing-3.svg" alt="" srcset="" />
       </div>
     </div>
@@ -128,7 +130,7 @@
         flex flex-col
         gap-2
         bg-footer
-        px-36
+        px-4 xl:px-36
         items-center
         justify-center
         text-light
@@ -136,7 +138,7 @@
       "
     >
       <div class="bg-black opacity-80 w-full h-full absolute"></div>
-      <div class="flex items-center z-10 gap-4">
+      <div class="flex flex-col xl:flex-row items-center z-10 xl:gap-4">
         <img src="./assets/logo-white.svg" alt="" srcset="" />
         <a href="#" target="_">
           <img src="./assets/playstore.svg" alt="" srcset=""
